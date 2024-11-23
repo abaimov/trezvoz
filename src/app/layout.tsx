d767e129b5718344bb15c66f482264ed868249f7
abaimov <w.abaimov@gmail.com>
2024-11-23 13:46:14 +0300

    fix ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,21 @@
-import type {Metadata} from "next";
-import {PT_Sans_Caption} from "next/font/google";
+import type { Metadata } from "next";
+import { PT_Sans_Caption } from "next/font/google";
 import "./globals.css";
 import StickyMenu from "@/app/Navbar";
 import ContainerProvider from "@/app/ContainerProvider";
-import {GoogleTagManager} from '@next/third-parties/google';
 import Script from "next/script";
 
 // Подключение шрифта PT Sans Caption
-const sans_caption = PT_Sans_Caption({subsets: ['cyrillic', 'latin'], weight: ['400', '700']});
+const sans_caption = PT_Sans_Caption({
+    subsets: ["cyrillic", "latin"],
+    weight: ["400", "700"],
+});
 
 // Метаданные страницы
 export const metadata: Metadata = {
     title: "🚗 Трезвый водитель Минск: Безопасное возвращение домой",
-    description: "🌟 Ваш надежный партнер на дороге — Trezvy 🚗. С нами вы всегда в безопасности, независимо от времени и места 🌃. Наши опытные водители гарантируют вам спокойствие и уверенность в том, что ваш путь домой будет таким же приятным, как и вечер 🏠.",
+    description:
+        "🌟 Ваш надежный партнер на дороге — Trezvy 🚗. С нами вы всегда в безопасности, независимо от времени и места 🌃. Наши опытные водители гарантируют вам спокойствие и уверенность в том, что ваш путь домой будет таким же приятным, как и вечер 🏠.",
 };
 
 // Главный компонент макета
@@ -23,84 +26,40 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <body className={sans_caption.className}>
-        {/* Google Analytics */}
-        <Script
-            id="google-analytics-js"
-            strategy="afterInteractive"
-            src="https://www.googletagmanager.com/gtag/js?id=8008797785"
-        />
-        <Script id="google-analytics-init" strategy="afterInteractive">
-            {`
+        <head>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link rel="icon" href="/favicon.ico" />
+
+            {/* Google Analytics */}
+            <Script
+                strategy="afterInteractive"
+                src="https://www.googletagmanager.com/gtag/js?id=G-D9NWJ5P27W"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '8008797785');
+            gtag('config', 'G-D9NWJ5P27W');
           `}
-        </Script>
-        <GoogleTagManager gtmId={"G-D9NWJ5P27W"}/>
-        <Script
-            id="gtag-js"
-            strategy="afterInteractive"
-            src="https://www.googletagmanager.com/gtag/js?id=G-D9NWJ5P27W"
-        />
-        <Script strategy="afterInteractive">
-            {`
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', 'G-D9NWJ5P27W');
-               `}
-        </Script>
-        {/* Яндекс Метрика */}
-        <Script id="yandex-metrika" strategy="afterInteractive">
-            {`(function(m,e,t,r,i,k,a){
-                        m[i]=m[i]||function(){
-                            (m[i].a=m[i].a||[]).push(arguments)
-                        }; 
-                        m[i].l=1*new Date(); 
-                        k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-                        k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-                    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-                    
-                    ym(97120978, "init", {
-                        defer: true,
-                        clickmap: true,
-                        trackLinks: true,
-                        accurateTrackBounce: true,
-                        webvisor: true
-                    });`}
-        </Script>
-        <noscript>
-            <div>
-                <img src="https://mc.yandex.ru/watch/97120978" style={{position: "absolute", left: "-9999px"}}
-                     alt="ya_metrika"/>
-            </div>
-        </noscript>
-
-        {/* Data Traffic Guard */}
-        <Script>
-            {`var dataTrafficGuard = dataTrafficGuard || [];
-                    dataTrafficGuard.push(['property', 'tg-013364-001']);
-                    dataTrafficGuard.push(['event', 'pageview']);
-                    (function() {
-                        var tg = document.createElement('script'); 
-                        tg.type = 'text/javascript'; 
-                        tg.async = true; 
-                        tg.src = '//tgtag.io/tg.js?pid=tg-013364-001';
-                        var s = document.getElementsByTagName('script')[0];
-                        s.parentNode.insertBefore(tg, s);
-                    })();`}
-        </Script>
-
+            </Script>
+        </head>
+        <body className={sans_caption.className}>
         {/* Основной контент приложения */}
         <ContainerProvider>
-            <StickyMenu/>
+            <StickyMenu />
             {children}
-            <div className={'text-center bg-black h-[50px]'}>
-                <a href="/politica" className={'uppercase font-bold text-[12px] underline text-white'}>Политика
-                    конфиденциальности</a>
-                <p className={'uppercase font-bold text-[8px] text-white'}>УНП 190010170</p>
+            <div className="text-center bg-black h-[50px]">
+                <a
+                    href="/politica"
+                    className="uppercase font-bold text-[12px] underline text-white"
+                >
+                    Политика конфиденциальности
+                </a>
+                <p className="uppercase font-bold text-[8px] text-white">
+                    УНП 190010170
+                </p>
             </div>
         </ContainerProvider>
         </body>
